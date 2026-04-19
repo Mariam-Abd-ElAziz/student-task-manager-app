6c1bd48420abffff5c727a06dc4a2c6ce8d7de5b
habiba alaa <habibaalaa775@gmal.com>
2026-04-19 03:50:54 +0200

    created firebase_options.dart  for connecting w firebase

---
+++ lib/firebase_options.dart
@@ -0,0 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
+
+class DefaultFirebaseOptions {
+  static FirebaseOptions get currentPlatform {
+    return const FirebaseOptions(
+      apiKey: "AIzaSyC1cEfyW5v9iWQO4yjbdcGtrRpgZDejEEQ",
+      authDomain: "student-task-manager-6f870.firebaseapp.com",
+      projectId: "student-task-manager-6f870",
+      storageBucket: "student-task-manager-6f870.firebasestorage.app",
+      messagingSenderId: "423267510973",
+      appId: "1:423267510973:web:b260d7cc22334b1099a5d4",
+    );
+  }
+}
