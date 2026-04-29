816d7dfe2f0fa9239a2ba1d4c1d551cf17c689e5
Habiba775 <125930000+Habiba775@users.noreply.github.com>
2026-04-29 03:19:46 +0300

    replaced id with user_id

--- lib/models/task.dart
+++ lib/models/task.dart
@@ -1,49 +1,47 @@
 class Task {
-  final int? id;
-  final String userId;
-  final String title;
-  final String? description;
-  final String dueDate;       
-  final String priority;      
+  String? id;
+  String title;
+  String? description;
+  String dueDate;
+  String priority;
   bool isCompleted;
-  bool isFavorite ;
+  bool isFavorite;
+  String userId;
 
- 
   Task({
     this.id,
-    required this.userId,
     required this.title,
     this.description,
     required this.dueDate,
     required this.priority,
-    this.isCompleted = false,
-    this.isFavorite = false,
+    required this.isCompleted,
+    required this.isFavorite,
+    required this.userId,
   });
- 
+
   Map<String, dynamic> toMap() {
     return {
-      if (id != null) 'id': id,
-      'user_id': userId,
-      'title': title,
-      'description': description,
-      'due_date': dueDate,
-      'priority': priority,
-      'is_completed': isCompleted ? 1 : 0,
-      'is_favorite': isFavorite ? 1 : 0,
+      "id": id,
+      "title": title,
+      "description": description,
+      "dueDate": dueDate,
+      "priority": priority,
+      "isCompleted": isCompleted,
+      "isFavorite": isFavorite,
+      "userId": userId,
     };
   }
- 
+
   factory Task.fromMap(Map<String, dynamic> map) {
     return Task(
-      id: map['id'] as int?,
-      userId: map['user_id'].toString() ,
-      title: map['title'] as String,
-      description: map['description'] as String?,
-      dueDate: map['due_date'] as String,
-      priority: map['priority'] as String,
-      isCompleted: (map['is_completed'] as int) == 1,
-      isFavorite: (map['is_favorite'] as int) == 1,
+      id: map["id"],
+      title: map["title"],
+      description: map["description"],
+      dueDate: map["dueDate"],
+      priority: map["priority"],
+      isCompleted: map["isCompleted"] ?? false,
+      isFavorite: map["isFavorite"] ?? false,
+      userId: map["userId"],
     );
   }
 }
- 
